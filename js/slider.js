cf2b05db5dc7d58fa6ea179dd399681999e53afd
Berdia <32432981+berdia21@users.noreply.github.com>
2023-03-23 22:32:17 +0400

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,31 +1,28 @@
 let interval = setInterval(() => {
   if (document.readyState === "complete" && document.querySelector(".swiper")) {
     clearInterval(interval);
-    setTimeout(() => {
-      let swiper = new Swiper(".swiper", {
-        autoHeight: true,
-        breakpoints: {
-          775: {
-            autoHeight: false,
-          },
+    let swiper = new Swiper(".swiper", {
+      autoHeight: true,
+      breakpoints: {
+        775: {
+          autoHeight: false,
         },
-      });
+      },
+    });
 
-      swiper.on("slideChange", function () {
-        document.querySelector(".slide-number").innerHTML =
-          swiper.activeIndex + 1;
-      });
-
+    swiper.on("slideChange", function () {
       document.querySelector(".slide-number").innerHTML =
         swiper.activeIndex + 1;
-      document.querySelector(".total-slides").innerHTML = swiper.slides.length;
+    });
 
-      document
-        .querySelector(".next-slide")
-        ?.addEventListener("click", () => swiper.slideNext());
-      document
-        .querySelector(".prev-slide")
-        ?.addEventListener("click", () => swiper.slidePrev());
-    }, 2000);
+    document.querySelector(".slide-number").innerHTML = swiper.activeIndex + 1;
+    document.querySelector(".total-slides").innerHTML = swiper.slides.length;
+
+    document
+      .querySelector(".next-slide")
+      ?.addEventListener("click", () => swiper.slideNext());
+    document
+      .querySelector(".prev-slide")
+      ?.addEventListener("click", () => swiper.slidePrev());
   }
 });

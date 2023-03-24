116bccae55fc5340f1cc7ea0bbc342a4bc44365e
Berdia <32432981+berdia21@users.noreply.github.com>
2023-03-24 11:36:17 +0400

    Add files via upload

--- js/fetch-components.js
+++ js/fetch-components.js
@@ -7,10 +7,13 @@ const fetchComponent = (path, targetElementId) => {
     );
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  fetchComponent("components/Header.html", "header");
-  fetchComponent("components/DesktopNavigation.html", "desktop-navigation");
-  fetchComponent("components/MobileNavigation.html", "mobile-navigation");
-  fetchComponent("components/Slider.html", "slider");
-  fetchComponent("components/News.html", "news");
-});
+let domCheck = setInterval(() => {
+  if (document.readyState === "complete" && document.querySelector("#news")) {
+    clearInterval(domCheck);
+    fetchComponent("components/Header.html", "header");
+    fetchComponent("components/DesktopNavigation.html", "desktop-navigation");
+    fetchComponent("components/MobileNavigation.html", "mobile-navigation");
+    fetchComponent("components/Slider.html", "slider");
+    fetchComponent("components/News.html", "news");
+  }
+}, 100);
